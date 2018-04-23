34f2381b6d631908087af99dcff7c24922301488
Derek Lutz <SpliceVW@spickle.int>
2018-04-22 22:31:48 -0400

    more initial setup stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-alert("hello world");
+const normalizeCss = require('normalize.css');
+const stylesheet = require('./scss/index.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
 
 module.exports = {
     entry: './src/index.ts',
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
@@ -28,6 +51,12 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Spickle.com',
             template: 'index.html'
-        })
+        }),
+        new CriticalPlugin({
+            src: 'index.html',
+            inline: true,
+            minify: true,
+            dest: 'index.html'
+          })
     ]
 };
