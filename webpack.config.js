52d860dccff9e4a063848648e1563244b980b942
Derek Lutz <SpliceVW@spickle.int>
2018-05-01 23:06:14 -0400

    added logo

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
+const FaviconsPlugin = require('favicons-webpack-plugin');
+const SriPlugin = require('webpack-subresource-integrity');
+
+const plugins = [new HtmlWebpackPlugin({
+    title: 'Spickle.com',
+    template: 'index.html',
+    filename: 'index.html'
+})];
+
 
 module.exports = {
+    context: __dirname,
     entry: './src/index.ts',
     mode: 'development',
     module: {
@@ -34,8 +44,19 @@ module.exports = {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     'file-loader'
+                    /*
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[hash].[ext]',
+                            outputPath: 'images/',
+                            publicPath: 'images/'
+                        }
+                    }
+                    */
                 ]
-            }
+            },
+            { test: /\.html$/, loader: 'html-loader' }
         ]
     },
     devServer: {
@@ -48,11 +69,21 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-//        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Spickle.com',
-            template: 'index.html'
+    plugins: this.mode === 'production' ?
+    [
+        new CleanWebpackPlugin(['dist']),
+        ...plugins,
+        new FaviconsPlugin({
+            logo: './src/images/derek.png',
+            prefix: 'favicons/icon[hash]',
+            inject: true,
+            emitStats: true,
+            statsFilename: 'iconstats-[hash].json',
+            title: 'Spickle.com'
+        }),
+        new SriPlugin({
+            enabled: process.env.NODE_ENV === 'production'
         })
-    ]
+    ] 
+    : [ ...plugins ]
 };
